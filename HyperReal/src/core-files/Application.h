429ff27bf376934b8377b21e642079ad653c6ef9
TusharGautam29 <tushar.gautam.23033@iitgoa.ac.in>
2025-01-13 23:59:57 +0530

    Rendering basic stuff

--- HyperReal/src/core-files/Application.h
+++ HyperReal/src/core-files/Application.h
@@ -31,6 +31,7 @@ namespace HyperR
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 	
+		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
--- HyperReal/src/core-files/Platform/OpenGL/OpenGLContext.cpp
+++ HyperReal/src/core-files/Platform/OpenGL/OpenGLContext.cpp
@@ -15,6 +15,13 @@ namespace HyperR {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		HR_CORE_ASSERT(status, "Failed to initialize Glad!");
+		
+		HR_CORE_INFO("OpenGL Info:");
+		HR_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+		HR_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+		HR_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
+
+
 	}
 	void OpenGLContext::SwapBuffers()
 	{
