4c0b802e46698fdd37440cd96a37a0656f5b74d2
TusharGautam29 <tushar.gautam.23033@iitgoa.ac.in>
2025-01-19 01:10:03 +0530

    Added transform to renderer

--- sandbox/src/SandboxApp.cpp
+++ sandbox/src/SandboxApp.cpp
@@ -1,6 +1,6 @@
 #include <HyperReal.h>
 #include "imgui/imgui.h"
-
+#include <glm/gtc/matrix_transform.hpp>
 
 
 class ExampleLayer : public HyperR::Layer
@@ -41,10 +41,10 @@ public:
 		m_SquareVA.reset(HyperR::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 		std::shared_ptr<HyperR::VertexBuffer> squareVB;
 
@@ -71,6 +71,7 @@ public:
 			layout(location = 0) in vec4 a_Color;		
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -78,7 +79,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 		std::string fragmentSrc = R"(
@@ -103,12 +104,13 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 		std::string blueShaderFragmentSrc = R"(
@@ -153,7 +155,17 @@ public:
 
 		HyperR::Renderer::BeginScene(m_Camera);
 
-		HyperR::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				HyperR::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+
 
 		HyperR::Renderer::Submit(m_Shader, m_VertexArray);
 
