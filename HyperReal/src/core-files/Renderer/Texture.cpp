46bdc715c5ee655a1d35f3dce12c574e2144fcb2
TusharGautam29 <tushar.gautam.23033@iitgoa.ac.in>
2025-02-02 02:17:47 +0530

    Both Textured and Non-Textured quads can now be drawn with one shader

--- HyperReal/src/core-files/Renderer/Texture.cpp
+++ HyperReal/src/core-files/Renderer/Texture.cpp
@@ -3,12 +3,21 @@
 #include "Renderer.h"
 #include "Platform/OpenGL/OpenGLTexture.h"
 namespace HyperR {
-	Ref<Texture2D> Texture2D::Create(const std::string& path)
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
 	{
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None:    HR_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL:  return std::make_shared<OpenGLTexture2D>(path);
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLTexture2D>(width,height);
+		}
+		HR_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}Ref<Texture2D> Texture2D::Create(const std::string& path)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:    HR_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL:  return CreateRef<OpenGLTexture2D>(path);
 		}
 		HR_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
